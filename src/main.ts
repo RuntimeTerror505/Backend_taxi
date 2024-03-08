22c3bd32880567cf71a65f0b1f57d5c1087eaccc
“vladyslav” <andriyilkiv322@gmail.com>
2024-03-08 17:46:16 +0200

    test

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 3000;
-  const app = await NestFactory.create(AppModule, { cors: {
-    origin: 'https://bookings.taxi',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
-    allowedHeaders: 'Content-Type, Accept', 
-  } });
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(PORT, ()=> console.log('server started on port '+PORT));
 }
 
