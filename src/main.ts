27a246f6fce7cd5d924cae232cf03c61e4f6dc4e
andriy <andriyilkiv322@gmail.com>
2024-03-15 16:33:19 +0200

    test

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 3000;
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(PORT, ()=> console.log('server started on port '+PORT));
 }
 
